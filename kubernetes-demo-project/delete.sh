596206966440af52528b04e2939c5dc2352fbb6c
desubhan <desubhan123@gmail.com>
2024-02-26 07:07:40 +0530

    Create start and delete shell scripts for managing Kubernetes resources
    Add start and delete shell scripts to apply and delete Kubernetes resources respectively for a demo project. The scripts provide an easy way to manage the resources as a group.

---
+++ kubernetes-demo-project/delete.sh
@@ -0,0 +1,4 @@
+kubectl delete -f mongo-express.yml
+kubectl delete -f mongodb.yml
+kubectl delete -f mongodb-secret.yml
+kubectl delete -f mongodb-configmap.yml
---
+++ kubernetes-demo-project/start.sh
@@ -0,0 +1,4 @@
+kubectl apply -f mongodb-secret.yml
+kubectl apply -f mongodb-configmap.yml
+kubectl apply -f mongodb.yml
+kubectl apply -f mongo-express.yml
